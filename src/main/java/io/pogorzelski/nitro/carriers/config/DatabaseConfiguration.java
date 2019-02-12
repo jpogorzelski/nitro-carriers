6c6f3ac12a934689515932381c196a919b48d02e
Jakub Pogorzelski <jakub.pogorzelski@cronn.de>
2019-02-12 17:04:11 +0100

    Add elasticsearch

--- src/main/java/io/pogorzelski/nitro/carriers/config/DatabaseConfiguration.java
+++ src/main/java/io/pogorzelski/nitro/carriers/config/DatabaseConfiguration.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -19,6 +20,7 @@ import java.sql.SQLException;
 @EnableJpaRepositories("io.pogorzelski.nitro.carriers.repository")
 @EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
 @EnableTransactionManagement
+@EnableElasticsearchRepositories("io.pogorzelski.nitro.carriers.repository.search")
 public class DatabaseConfiguration {
 
     private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
