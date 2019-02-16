f5192f86867cd112cb2dd6df5e8814c559da4a8f
Jakub Pogorzelski <jakub.pogorzelski@cronn.de>
2019-02-16 15:25:50 +0100

    Minor fixes

--- src/main/webapp/app/entities/nitro-rating/nitro-rating-detail.component.ts
+++ src/main/webapp/app/entities/nitro-rating/nitro-rating-detail.component.ts
@@ -1,8 +1,23 @@
-import { RatingComponent, RatingDetailComponent } from 'app/entities/rating';
-import { Component } from '@angular/core';
+import { RatingDetailComponent } from 'app/entities/rating';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from 'app/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'jhi-nitro-rating-detail',
     templateUrl: './nitro-rating-detail.component.html'
 })
-export class NitroRatingDetailComponent extends RatingDetailComponent {}
+export class NitroRatingDetailComponent extends RatingDetailComponent implements OnInit {
+    currentAccount: any;
+
+    constructor(protected accountService: AccountService, protected activatedRoute: ActivatedRoute) {
+        super(activatedRoute);
+    }
+
+    ngOnInit() {
+        super.ngOnInit();
+        this.accountService.identity().then(account => {
+            this.currentAccount = account;
+        });
+    }
+}
