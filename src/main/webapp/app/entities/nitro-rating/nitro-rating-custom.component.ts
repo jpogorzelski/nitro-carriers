d2c1ae4d74fe2a552181d0f28c35e14b7c8cf7e9
Jakub Pogorzelski <jakub.pogorzelski@cronn.de>
2019-08-01 13:39:57 +0200

    Increase page size for black/white list

--- src/main/webapp/app/entities/nitro-rating/nitro-rating-custom.component.ts
+++ src/main/webapp/app/entities/nitro-rating/nitro-rating-custom.component.ts
@@ -1,15 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { Grade } from 'app/shared/model/rating.model';
 import { NitroRatingComponent } from 'app/entities/nitro-rating/nitro-rating.component';
+import { ITEMS_PER_PAGE } from 'app/shared';
 
 @Component({
     selector: 'jhi-carrier-ratings-custom',
-     templateUrl: './nitro-rating.component.html'
+    templateUrl: './nitro-rating.component.html'
 })
 export class NitroRatingCustomComponent extends NitroRatingComponent {
 
     @Input()
     query: any = {};
+
+    @Input()
+    itemsPerPage = ITEMS_PER_PAGE;
 
     filter() {
         const whitelist = this.query.whitelist;
