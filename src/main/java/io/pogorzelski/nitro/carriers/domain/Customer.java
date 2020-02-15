0f06804eda4c4e454f68913f10785f8c5b266503
Jakub Pogorzelski <qba.pogorzelski@gmail.com>
2020-02-15 21:42:25 +0100

    Introduce new entity Customer

---
+++ src/main/java/io/pogorzelski/nitro/carriers/domain/Customer.java
@@ -0,0 +1,188 @@
+package io.pogorzelski.nitro.carriers.domain;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+import org.springframework.data.elasticsearch.annotations.Document;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A Customer.
+ */
+@Entity
+@Table(name = "customer")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+@Document(indexName = "customer")
+public class Customer implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(name = "nip", nullable = false, unique = true)
+    private String nip;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "postal_code")
+    private String postalCode;
+
+    @ManyToOne
+    @JsonIgnoreProperties("customers")
+    private City city;
+
+    @ManyToOne
+    @JsonIgnoreProperties("customers")
+    private Country country;
+
+    @ManyToOne
+    @JsonIgnoreProperties("customers")
+    private User user;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Customer name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNip() {
+        return nip;
+    }
+
+    public Customer nip(String nip) {
+        this.nip = nip;
+        return this;
+    }
+
+    public void setNip(String nip) {
+        this.nip = nip;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Customer address(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public Customer postalCode(String postalCode) {
+        this.postalCode = postalCode;
+        return this;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public Customer city(City city) {
+        this.city = city;
+        return this;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public Customer country(Country country) {
+        this.country = country;
+        return this;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Customer user(User user) {
+        this.user = user;
+        return this;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Customer customer = (Customer) o;
+        if (customer.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), customer.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+            "id=" + getId() +
+            ", name='" + getName() + "'" +
+            ", nip='" + getNip() + "'" +
+            ", address='" + getAddress() + "'" +
+            ", postalCode='" + getPostalCode() + "'" +
+            "}";
+    }
+}
