5cba9c426265f00bdd9f4ac039ad63d884401c7a
Jakub Pogorzelski <jakub.pogorzelski@cronn.de>
2020-02-18 18:02:56 +0100

    Add State field with possible values: available, taken, temporarily_taken

--- src/main/webapp/app/shared/model/customer.model.ts
+++ src/main/webapp/app/shared/model/customer.model.ts
@@ -2,12 +2,19 @@ import { ICity } from 'app/shared/model/city.model';
 import { ICountry } from 'app/shared/model/country.model';
 import { IUser } from 'app/core/user/user.model';
 
+export const enum CustomerState {
+    AVAILABLE = 'AVAILABLE',
+    TAKEN = 'TAKEN',
+    TEMPORARILY_TAKEN = 'TEMPORARILY_TAKEN'
+}
+
 export interface ICustomer {
     id?: number;
     name?: string;
     nip?: string;
     address?: string;
     postalCode?: string;
+    state?: CustomerState;
     city?: ICity;
     country?: ICountry;
     user?: IUser;
@@ -20,6 +27,7 @@ export class Customer implements ICustomer {
         public nip?: string,
         public address?: string,
         public postalCode?: string,
+        public state?: CustomerState,
         public city?: ICity,
         public country?: ICountry,
         public user?: IUser
