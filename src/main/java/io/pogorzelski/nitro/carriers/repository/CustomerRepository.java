45ff846ffa1f150ba0b077dc18242df9227611b4
Jakub Pogorzelski <jakub.pogorzelski@cronn.de>
2020-02-18 18:52:41 +0100

    Security measures: GET /customers - all for ADMIN, assigned or state == AVAILABLE for others GET /customers/id - ok for ADMIN. For all others only if state == AVAILABLE or assigned to user PUT /customers/id - ok for ADMIN. For all others only if state == AVAILABLE or assigned to user
    TODO:
    DELETE, search

--- src/main/java/io/pogorzelski/nitro/carriers/repository/CustomerRepository.java
+++ src/main/java/io/pogorzelski/nitro/carriers/repository/CustomerRepository.java
@@ -1,10 +1,13 @@
 package io.pogorzelski.nitro.carriers.repository;
 
 import io.pogorzelski.nitro.carriers.domain.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 /**
  * Spring Data  repository for the Customer entity.
@@ -13,7 +16,11 @@ import java.util.List;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-    @Query("select customer from Customer customer where customer.user.login = ?#{principal.username}")
-    List<Customer> findByUserIsCurrentUser();
+    @Query("select customer from Customer customer where customer.user.login = ?#{principal.username} or customer.state = io.pogorzelski.nitro.carriers.domain.enumeration.CustomerState.AVAILABLE")
+    Page<Customer> findByUserIsCurrentUserOrAvailable(Pageable pageable);
+
+    @Query("select customer from Customer customer " +
+        "where customer.id = :id and customer.user.login = ?#{principal.username}")
+    Optional<Customer> findByIdAndUserIsCurrentUser(@Param("id") Long id);
 
 }
